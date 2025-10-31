353aa908b696818e6eca6c6c23fc0a7618134e8f
nur-nayeem <nurnayem768@gmail.com>
2025-10-31 15:46:49 +0600

    added login and signup page

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { Link } from "react-router";
+
+const LoginPage = () => {
+  return (
+    <div className="mx-auto bg-white p-10 space-y-3.5">
+      <h3 className="text-center text-3xl font-semibold rounded-lg">Login</h3>
+      <p className="text-center">
+        Don't have and account?{" "}
+        <Link to={"/auth/register"} className="text-primary">
+          Register Now
+        </Link>
+      </p>
+      <div>
+        <form className="flex flex-col max-w-md sm:w-md gap-6">
+          <div className="flex flex-col space-y-1.5 ">
+            <label>Email</label>
+            <input
+              className="bg-base-200 p-2.5 rounded-lg 
+              outline-2 outline-gray-200 focus:outline-primary"
+              type="email"
+              placeholder="Email"
+            />
+          </div>
+          <div className="flex flex-col space-y-1.5 ">
+            <label>Password</label>
+            <input
+              className="bg-base-200 p-2.5 rounded-lg outline-2 outline-gray-200 focus:outline-primary"
+              type="password"
+              placeholder="Password"
+            />
+            <Link className="mt-1" to={"/auth/forget-password"}>
+              Forgot Password?
+            </Link>
+          </div>
+
+          <button className="btn bg-primary text-base-100 rounded-lg text-lg h-12">
+            Sign In
+          </button>
+        </form>
+        <div className="flex items-center justify-between my-5">
+          <hr className="text-gray-300 w-[45%]" />
+          <span>OR</span>
+          <hr className="text-gray-300 w-[45%]" />
+        </div>
+
+        <button className="btn bg-white text-black border-[#e5e5e5] w-full rounded-lg h-12 text-lg">
+          <svg
+            aria-label="Google logo"
+            width="20"
+            height="20"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+          >
+            <g>
+              <path d="m0 0H512V512H0" fill="#fff"></path>
+              <path
+                fill="#34a853"
+                d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+              ></path>
+              <path
+                fill="#4285f4"
+                d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+              ></path>
+              <path
+                fill="#fbbc02"
+                d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+              ></path>
+              <path
+                fill="#ea4335"
+                d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+              ></path>
+            </g>
+          </svg>
+          Login with Google
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
